56cf4ef1fdbbc1b6e45f3fadd684e273f818424b
JANG SAN <mocakosan@naver.com>
2022-06-16 00:14:25 +0900

    hw 2주차 recoil 작업중

---
+++ magazine/src/recoil/store.js
@@ -0,0 +1,19 @@
+import {atom, selector} from "recoil";
+import {recoilPresist} from "recoil-persist";
+export const board = atom({
+  board_id:4,
+  imageLink:"https://hanghae-bucket.s3.ap-northeast-2.amazonaws.com/mydir/d71dc94e-e431-486c-bf8f-9b9e033aa2789c19aa4b397f3563408dd414a7ef84f5.jpg",
+  content:"front",
+  user: "js",
+  layout: "1",
+  like:"1",
+  createdAt:"2022-06-15T10:29:34.174356",
+  updatedAt:"2022-06-15T10:29:34.174356",
+});
+
+export const getBoard = selector({
+  key: "getBoard",
+  get: ({get}) => {
+    return get(board);
+  }
+})
