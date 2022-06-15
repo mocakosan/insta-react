0aa44b667926ca30ff86daa966f14d37ad67d73c
JANG SAN <mocakosan@naver.com>
2022-06-16 02:32:37 +0900

    hw2주차 api데이터 블러오기 성공

--- magazine/src/index.js
+++ magazine/src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {RecoilRoot} from "recoil";
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <App/>
+    </RecoilRoot>
+   </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
